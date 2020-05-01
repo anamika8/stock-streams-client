9d39c0c158890c012099726a422efbeaac07cbab
anamika8 <anamikaeight@gmail.com>
2020-04-30 21:30:04 -0700

    added responsive for first page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Search from "./Components/Search.jsx";
 import Feed from "./Components/Feed.jsx";
 import Count from "./Components/Count.jsx";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import "./main.css";
 
 import { Container, Col, Row, Navbar } from "react-bootstrap";
 
@@ -27,22 +29,16 @@ export default function App() {
       <br />
       <Count data={data} />
       <br />
-      <Container>
-        <Row>
-          <Col>
-            {data.length == 0 ? (
-              <img
-                src="https://www.belltraininggroup.com.au/wp-content/uploads/2017/02/Business_Success.jpg"
-                alt="Stock-exchange"
-                className="img-stock"
-              />
-            ) : (
-              <></>
-            )}
-            <Feed data={data}></Feed> />
-          </Col>
-        </Row>
-      </Container>
+
+      {data.length == 0 ? (
+        <img
+          src="https://www.belltraininggroup.com.au/wp-content/uploads/2017/02/Business_Success.jpg"
+          alt="Stock-exchange"
+          className="img-stock"
+        />
+      ) : (
+        <Feed data={data}></Feed>
+      )}
     </div>
   );
 }
